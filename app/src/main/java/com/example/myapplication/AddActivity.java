019e7d108d0ee129aede1aadf3ae51f7c578a7ea
samgashyeong <samgashyeong@naver.com>
2020-10-30 21:43:48 +0900

    bottom view

--- app/src/main/java/com/example/myapplication/AddActivity.java
+++ app/src/main/java/com/example/myapplication/AddActivity.java
@@ -4,13 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 public class AddActivity extends AppCompatActivity {
+
+    private EditText plan_edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +31,19 @@ public class AddActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        plan_edit=(EditText)findViewById(R.id.add_plan);
         switch (item.getItemId()){
-            case R.id.action_btn{
-                break;
+            case R.id.action_btn :{
+                String plan=plan_edit.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), AddFragment.class);
+                intent.putExtra("plan", plan);
+
+                setResult(RESULT_OK, intent);
+                return true;
             }
-            case R.id.cancel_btn{
-                break;
+            case R.id.cancel_btn :{
+                setResult(RESULT_CANCELED);
+                finish();
             }
         }
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/example/myapplication/AddFragment.java
+++ app/src/main/java/com/example/myapplication/AddFragment.java
@@ -1,18 +1,26 @@
 package com.example.myapplication;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import static android.content.ContentValues.TAG;
 
 public class AddFragment extends Fragment {
 
     private Button plan_btn;
+    public  static  final int REQUEST_CODE_=1000;
+    private String plan_str;
 
     public AddFragment() {
         // Required empty public constructor
@@ -34,10 +42,24 @@ public class AddFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 Intent Intent = new Intent(v.getContext(), AddActivity.class);
-                startActivity(Intent);
+                startActivityForResult(Intent, REQUEST_CODE_);
             }
         });
         return v;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_) {
+            if(resultCode==Activity.RESULT_OK) {
+                plan_str=data.getStringExtra("plan");
+
+                Log.d(TAG,"onActivity"+plan_str+",");
+                Toast.makeText(getActivity(), "저장되었습니다.", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
 
