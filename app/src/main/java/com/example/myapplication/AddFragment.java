8e24fb89402ab12c52e3351aece548743ca0ae66
samgashyeong <samgashyeong@naver.com>
2020-11-04 01:42:52 +0900

    fix bug+Alert+Alarm(50%)

--- app/src/main/java/com/example/myapplication/AddFragment.java
+++ app/src/main/java/com/example/myapplication/AddFragment.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -12,7 +13,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -29,9 +32,11 @@ import static android.content.ContentValues.TAG;
 public class AddFragment extends Fragment {
 
     private Button plan_btn;
-    private String plan_str;
     private TextView PlanTextView;
-
+    private EditText AddPlan;
+    private Button CheckBtn;
+    public String s="아직 목표가 입력되지않았습니다.";
+    private InputMethodManager imm;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -45,13 +50,34 @@ public class AddFragment extends Fragment {
         View v = (ViewGroup) inflater.inflate(R.layout.fragment_add,container,false);
         plan_btn=(Button) v.findViewById(R.id.plan_add_btn);
         PlanTextView=(TextView) v.findViewById(R.id.planText);
+        AddPlan=(EditText) v.findViewById(R.id.PlanAdd);
+        CheckBtn=(Button) v.findViewById(R.id.CheckBtn);
+
+
         plan_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent Intent = new Intent(v.getContext(), AddActivity.class);
-                startActivityForResult(Intent, 1000);
+                CheckBtn.setVisibility(View.VISIBLE);
+                AddPlan.setVisibility(View.VISIBLE);
+                PlanTextView.setVisibility(View.INVISIBLE);
+                plan_btn.setVisibility(View.INVISIBLE);
             }
         });
+
+        CheckBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                s=AddPlan.getText().toString();
+                PlanTextView.setText(s);
+                CheckBtn.setVisibility(View.INVISIBLE);
+                AddPlan.setVisibility(View.INVISIBLE);
+                PlanTextView.setVisibility(View.VISIBLE);
+                plan_btn.setVisibility(View.VISIBLE);
+            }
+        });
+        PlanTextView.setText(s);
         return v;
     }
+
+
 }
--- app/src/main/java/com/example/myapplication/SettingFragment.java
+++ app/src/main/java/com/example/myapplication/SettingFragment.java
@@ -1,21 +1,30 @@
 package com.example.myapplication;
 
-import android.app.TimePickerDialog;
+import android.app.AlarmManager;
+import android.app.AlertDialog;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TimePicker;
+
+import static android.content.Context.ALARM_SERVICE;
+
 
 public class SettingFragment extends Fragment {
 
     Button ReTimeBtn;
     Button ResetPlanBtn;
     Button DevoleperBtn;
+    TimePicker TimePicker;
 
 
     public SettingFragment() {
@@ -31,44 +40,58 @@ public class SettingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View v = (ViewGroup) inflater.inflate(R.layout.fragment_add,container,false);
+        View v = (ViewGroup) inflater.inflate(R.layout.fragment_setting,container,false);
         ReTimeBtn=v.findViewById(R.id.planReTime);
         ResetPlanBtn=v.findViewById(R.id.planReset);
         DevoleperBtn=v.findViewById(R.id.devolper);
+        TimePicker=v.findViewById(R.id.TimePicker);
+
 
         ReTimeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                TimePicker.setVisibility(View.VISIBLE);
+                ReTimeBtn.setVisibility(View.INVISIBLE);
+                DevoleperBtn.setVisibility(View.INVISIBLE);
+                ResetPlanBtn.setVisibility(View.INVISIBLE);
+                
             }
         });
 
-//        ReTimeBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//            }
-//        });
-//
-//
-//        ResetPlanBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                ReTimeBtn.setText("시간 재설정");
-//            }
-//        });
-//
-//        devoleperBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//            }
-//        });
+
+        ResetPlanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ReTimeBtn.setText("시간 재설정");
+            }
+        });
+
+        DevoleperBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+                builder.setTitle("만든 사람들")
+                        .setMessage("개발자 : 민경록 김서윤 이준상\n" +
+                                "디자이너 : 이소윤 이지수")
+                        .setNegativeButton("취소", null);
+                AlertDialog alert=builder.create();
+                alert.show();
+            }
+        });
         return v;
     }
 
-    public  void showAlarmDialog(View view){
-        TimePickerFragment timePickerFragment = new TimePickerFragment();
-        timePickerFragment.show(getFragmentManager(),"timePicker");
-    }
+//    public void testAlarm() {
+//        Intent intent = new Intent(getContext(), AlarmReceiver.class);
+//        PendingIntent sender = PendingIntent.getBroadcast(getContext(), 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+//        AlarmManager am = (AlarmManager) ContextCompat.getSystemService(getContext());
+//        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.N){
+//            am.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP,System.currentTimeMillis()+1000, sender);
+//        } else{
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//                am.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+1000, sender);
+//            }
+//        }
+//    }
 }
